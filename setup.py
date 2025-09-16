97d3b5db6033b06daa7c11527f40c5826a574a5c
Srimman Maheswaran <srimmanmaheswaran@Srimmans-MacBook-Air.local>
2025-09-16 13:50:49 +0530

    added env hooks

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ data_files_list = [
     (f'share/{package_name}', ['package.xml']),
     (f'share/{package_name}/config', config_files),
     (f'share/{package_name}/rviz', rviz_files),
+    (f'share/{package_name}/env-hooks', ['env-hooks/iris.dsv.in']),
 ]
 
 for dest_dir, files in data_files_dict.items():
